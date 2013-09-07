362f59e7a9a2d7a116f4ae44bbf0f6c08e21e1f3
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2013-09-07 22:50:41 +0100

    rough finish

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.locals.pretty = true;   // format source code
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -29,6 +30,44 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.get('/riddle1', routes.riddle1);
+app.post('/riddle1', routes.riddle1);
+
+app.get('/riddle2', routes.riddle2);
+app.post('/riddle2', routes.riddle2);
+
+app.get('/riddle3', routes.riddle3);
+app.post('/riddle3', routes.riddle3);
+
+app.get('/ziggy', routes.riddle4);
+app.post('/ziggy', routes.riddle4);
+
+app.get('/HID', routes.hid);
+app.post('/HID', routes.hid);
+
+app.get('/charades', routes.riddle6);
+app.post('/charades', routes.riddle6);
+
+app.get('/1984', routes.riddle7);
+app.post('/1984', routes.riddle7);
+
+app.get('/eight', routes.riddle8);
+app.post('/eight', routes.riddle8);
+
+app.get('/location', routes.riddle9);
+app.post('/location', routes.riddle9);
+
+app.get('/l4s7', routes.riddle10);
+app.post('/l4s7', routes.riddle10);
+
+app.get('/ultimate', routes.end);
+app.post('/ultimate', routes.end);
+
+app.get('/theanswer', routes.theanswer); // rickroll
+
+// app.get('/riddle5', routes.riddle5);
+// app.post('/riddle5', routes.riddle5);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,106 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PiHack' });
 };
+
+
+exports.riddle1 = function(req, res){
+  if (req.method == "POST") {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username == "admin" && password == "9876") {
+      // add banner
+      res.render('riddle2', { title: 'Riddle 2', icon: "icon-thumbs-up", status: 'Correct! That was easy enough...' });
+    } else {
+      // add banner
+      res.render('riddle1', { title: 'Riddle 1', icon: 'icon-thumbs-down', status: 'Incorrect!' });
+    };
+  };
+
+  res.render('riddle1', { title: 'Riddle 1', icon: 'icon-bullhorn', status: 'Welcome!' });
+};
+
+exports.riddle2 = function(req, res){
+  if (req.method == "POST") {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username == "admin" && password == "9876") {
+      // add banner
+      res.render('riddle3', { title: 'Riddle 3', icon: 'icon-thumbs-up', status: 'Correct! Again... Your luck wont continue.' });
+    } else {
+      // add banner
+      res.render('riddle2', { title: 'Riddle 2', icon: 'icon-thumbs-down', status: 'Incorrect!' });
+    };
+  };
+  // var status = 'Correct!';
+
+  res.render('riddle2', { title: 'Riddle 2', icon: "icon-thumbs-up", status: 'Correct! That was easy enough...' });
+};
+
+exports.riddle3 = function(req, res){
+  res.end('riddle3');
+};
+
+exports.riddle4 = function(req, res){
+
+  res.render('riddle4', { title: 'Riddle 4' });
+};
+
+exports.theanswer = function(req, res){
+  res.redirect('http://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  // rickroll
+};
+
+exports.hid = function(req, res){
+  res.render('riddle5', { title: 'Riddle 5' });
+}
+
+exports.riddle6 = function(req, res){
+
+  if (req.method == 'POST') {
+    var first = req.body.first;
+    var second = req.body.second;
+
+    if (first == 'spider' || first == 'Spider' && second == 'man' || second == 'Man') {
+      res.render('riddle6'), { title: 'Riddle 6', first: 'Spider', second: 'Man', icon: 'icon-thumbs-up', status: 'Success! Yeah yeah... I know it\'s actually one word...' };
+    } else {
+      res.render('riddle5', { title: 'Riddle 5', icon: 'icon-thumbs-down', status: 'Incorrect!' });
+    }
+  } else {
+    res.render('riddle6'), { title: 'Riddle 6', first: 'Spider', second: 'Man', icon: 'icon-thumbs-up', status: 'Success! Yeah yeah... I know it\'s actually one word...' };
+    // res.render('riddle5', { title: 'Riddle 5', icon: 'icon-thumbs-down', status: 'Incorrect!' });
+  }
+}
+
+exports.riddle7 = function(req, res){
+  res.render('riddle7', { title: 'Riddle 7' });
+}
+
+exports.riddle8 = function(req, res){
+  res.render('riddle8', { title: 'Riddle 8', icon: 'icon-smile', status: 'Success! That\s really kind of you, how nice...' })
+}
+
+exports.riddle9 = function(req, res){
+  res.render('riddle9');
+}
+
+exports.riddle10 = function(req, res){
+
+
+  res.render('riddle10');
+}
+
+exports.end = function(req, res){
+  if (req.method == "POST") {
+    var username = req.body.username;
+    var password = req.body.password;
+
+
+    if (password == 'security') {
+      res.render('ultimate', { name: username, title: 'END', icon: 'icon-smile', status: 'Success! Good job!' });
+    };
+
+  };
+  res.render('riddle10', { title: 'Riddle 10', icon: 'icon-thumbs-down', status: 'Incorrect! Try again.' });
+}
